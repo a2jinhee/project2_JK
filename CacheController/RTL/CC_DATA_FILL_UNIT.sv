6280125aeff9758c06f2bd6b4d77aeb6113e94ba
Jinhee Kim <a2jinhee@gmail.com>
2024-05-26 14:38:23 +0900

    FILL: update

--- CacheController/RTL/CC_DATA_FILL_UNIT.sv
+++ CacheController/RTL/CC_DATA_FILL_UNIT.sv
@@ -31,7 +31,7 @@ module CC_DATA_FILL_UNIT
     reg     [511:0]         wdata_data;
     reg     [2:0]           cnt, cnt_n;
     reg     [2:0]           wrptr;
-    reg                     enable, enable_n;
+    reg                     enable;
     reg     [2:0]           offset, offset_n;
 
     // State machine flip-flop
@@ -42,7 +42,6 @@ module CC_DATA_FILL_UNIT
             wdata_tag       <= 18'b0;
             offset          <= 3'b0;
             miss_addr_fifo_rden = 1'b0;
-            enable          <= 1'b0;
         end     
         else begin
             cnt             <= cnt_n;
@@ -50,7 +49,6 @@ module CC_DATA_FILL_UNIT
             wdata_tag       <= wdata_tag_n;
             offset          <= offset_n;
             miss_addr_fifo_rden = miss_addr_fifo_rden_n;
-            enable          <= enable_n;
         end 
     end 
 
@@ -58,15 +56,14 @@ module CC_DATA_FILL_UNIT
     always_comb begin
         // Latch problem 
         miss_addr_fifo_rden_n = miss_addr_fifo_rden;
-        enable = enable_n;
 
         // Determine miss_addr_fifo_rden // IMPORTANT
         if (mem_rvalid_i & mem_rready_i & (cnt=='b0))   miss_addr_fifo_rden_n =1'b1;
         else if ((cnt!=0))                              miss_addr_fifo_rden_n = 1'b0;
         
         // Determine enable // IMPORTANT
-        if (miss_addr_fifo_rden_n)    enable_n = 1'b1;
-        else if (cnt_n==7)          enable_n = 1'b0;
+        if (miss_addr_fifo_rden_n)    enable <= 1'b1;
+        else if (cnt_n==7)          enable <= 1'b0;
 
 
         // When miss_addr_fifo_rden==1, pop addr data and divide to addr, tag, offset
@@ -81,7 +78,7 @@ module CC_DATA_FILL_UNIT
         wrptr = (offset_n+cnt) % 8;
 
         // Choose the data to write: Deserialize the data
-        if(enable_n) begin
+        if(enable) begin
             if(wrptr==0)        wdata_data[63:0]    = mem_rdata_i;
             else if(wrptr==1)   wdata_data[127:64]  = mem_rdata_i;
             else if(wrptr==2)   wdata_data[191:128] = mem_rdata_i;
@@ -98,7 +95,7 @@ module CC_DATA_FILL_UNIT
             wren    = 'd1;
             cnt_n   = 'd0;
         end
-        else if(enable_n)begin
+        else if(enable)begin
             wren    = 'd0;
             cnt_n   = cnt+1;
         end
