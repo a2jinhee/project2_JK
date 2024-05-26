8a090db19369557a09f8db773c4828e7d6188d11
Jinhee Kim <a2jinhee@gmail.com>
2024-05-26 13:49:56 +0900

    REORDER: update

--- CacheController/RTL/CC_DATA_REORDER_UNIT.sv
+++ CacheController/RTL/CC_DATA_REORDER_UNIT.sv
@@ -98,6 +98,7 @@ module CC_DATA_REORDER_UNIT
             && ( (!hit_flag_fifo_rdata&&mem_rvalid_i) || (hit_flag_fifo_rdata && serializer_rvalid) ) 
             && (cnt=='b0))                      hit_flag_fifo_rden  = 1'b1;
         else if ((cnt!=0))                      hit_flag_fifo_rden = 1'b0;
+        else hit_flag_fifo_rden = 1'b0;
 
         // Increment cnt for bursting
         if (cnt==7)                                 cnt_n = 'b0;
@@ -106,10 +107,8 @@ module CC_DATA_REORDER_UNIT
         // Determine serializer_rready and mem_rready (output of DATA REORDER UNIT) // IMPORTANT
         if (!hit_flag_fifo_rdata & hit_flag_fifo_rden)      mem_rready          = 1'b1;
         else if (mem_rlast)                                 mem_rready          = 1'b0;
-        else mem_rready = 1'b0;
         if (hit_flag_fifo_rdata & hit_flag_fifo_rden)       serializer_rready   = 1'b1;
         else if (serializer_rlast)                          serializer_rready   = 1'b0;
-        else serializer_rready = 1'b0;
 
         // Mux between data/last from [MC R Channel] & [Data FIFO]
         if(hit_n)begin 
